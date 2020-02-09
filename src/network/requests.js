8ef0f74f6885c25ad165edec7504fd985f921b19
muxiaojue <545214351@qq.com>
2020-02-09 13:07:15 +0800

    fix

--- src/network/requests.js
+++ src/network/requests.js
@@ -2,8 +2,8 @@ import Axios from 'axios'
 
 export function request(config){
   const instance = new Axios.create({
-    // baseURL: 'http://123.207.32.32:8000/api/hy',
-    baseURL: 'http://106.54.54.237:8000/api/hy',
+    baseURL: 'http://123.207.32.32:8000',
+    //真实地址请联系微信coderwhy001
     timeout: 4000
   })
 
